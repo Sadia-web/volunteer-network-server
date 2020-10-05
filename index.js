2036aa35d3dd5154ddf6cc6777166ee3fce1e0be
Sadia-web <sadiamim354@gmail.com>
2020-10-05 15:13:15 +0600

    heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectID;
 require('dotenv').config()
 
 
@@ -10,10 +11,12 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 const app = express()
 const port = 5000
+
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true });
@@ -22,13 +25,50 @@ client.connect(err => {
 
   app.post('/addWork', (req, res) => {
       const work = req.body;
-      workCollection.insertOne(work)
+      workCollection.insertMany(work)
       .then(result => {
-          console.log(result)
+          res.send(result.insertedCount)
       })
   })
-  
-});
+
+  app.post("/registeredEvent", (req, res) => {
+      const latestEvent = req.body;
+      RegisteredEventCollection.insertOne(latestEvent)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+
+  })
+
+  app.get('/' , (req, res) => {
+    res.send("ready to Work")
+  })
+
+  app.get('/work', (req, res) => {
+    workCollection.find({})
+    .toArray((err, documents) => {
+        res.send(documents);
+    })
+  })
+
+  app.get('/registeredEvent', (req, res) => {
+    RegisteredEventCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents);
+    })
+  })
+
+  app.delete('/delete/:id', (req, res) => {
+    console.log(req.params.id)
+    RegisteredEventCollection.deleteOne({_id: ObjectId(req.params.id)})
+    .then(result => {
+      console.log(result);
+      res.send(result.deletedCount > 0);
+    })
+})
+     
+
+  });
 
 
-app.listen(port)
+app.listen(process.env.PORT || port)
